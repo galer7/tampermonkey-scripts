eefa1e9c4a72ed45e5c6ebb84745a1fa3da51fbb
Gabriel <gabigaler50@gmail.com>
2023-11-06 11:30:22 +0200

    Fix iflow script

--- src/iflow.ts
+++ src/iflow.ts
@@ -10,65 +10,90 @@
 // ==/UserScript==
 
 (function () {
-  function wait(ms: number) {
-    return new Promise(function (resolve, reject) {
-      setTimeout(resolve, ms);
-    });
-  }
-
-  async function tsClick(element: HTMLElement | null) {
-    if (!element) throw new Error(`Element does not exist!`);
-    element.click();
-    await wait(200);
-  }
-
   window.onload = async function () {
     await wait(1000);
 
     // Open modal
-    await tsClick(
+    await clickAndWait(
       document.querySelector(
         "#app > div.td-router-view > div > div > div.col-md-10.td-user-page-main > div.td-user-day-data > div:nth-child(3) > div > div > div.row.td-user-att-header > div > div.td-add-live-att-btn-wrap > a"
       )
     );
 
     // Open location picker
-    await tsClick(
+    await clickAndWait(
       document.querySelector(
         "#app > div.td-router-view > div > div > div.col-md-10.td-user-page-main > div.td-user-day-data > div:nth-child(3) > div > div > div.td-user-attendance-list-wrap > div > div.td-checkin-modal > div > div > div > div.modal-body > div > form > div:nth-child(2) > div > div > div > div > div.td-select-single-button"
       )
     );
 
     // Pick location: Home
-    await tsClick(
+    await clickAndWait(
       document.querySelector(
         "#app > div.td-router-view > div > div > div.col-md-10.td-user-page-main > div.td-user-day-data > div:nth-child(3) > div > div > div.td-user-attendance-list-wrap > div > div.td-checkin-modal > div > div > div > div.modal-body > div > form > div:nth-child(2) > div > div > div > div > div.form-group.td-select-list > div.td-elements-list > div.td-elements-list-wrap > div:nth-child(2) > a.td-element.td-parent-element"
       )
     );
 
     // Open start time picker
-    await tsClick(document.querySelector("#td-start-time"));
+    await clickAndWait(
+      document.querySelector("[id*='td-ckeck-in-out-start-time'")
+    );
 
     // Pick start time: 9AM
-    await tsClick(
-      document.querySelector(
-        "body > div.ui-timepicker-wrapper.shepherd-target > ul > li:nth-child(19)"
-      )
-    );
+    await pickStartTime();
 
     // Open end time picker
-    await tsClick(document.querySelector("#td-end-time"));
-
-    // Pick end time: 5PM
-    await tsClick(
-      document.querySelector("body > div:nth-child(12) > ul > li:nth-child(35)")
+    await clickAndWait(
+      document.querySelector("[id*='td-ckeck-in-out-end-time'")
     );
 
+    // Pick end time: 5PM
+    await pickEndTime();
+
     // Submit form with click
-    await tsClick(
+    await clickAndWait(
       document.querySelector(
         "#app > div.td-router-view > div > div > div.col-md-10.td-user-page-main > div.td-user-day-data > div:nth-child(3) > div > div > div.td-user-attendance-list-wrap > div > div.td-checkin-modal > div > div > div > div.modal-footer > button"
       )
     );
   };
 })();
+
+function wait(ms: number) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function clickAndWait(element: HTMLElement | null) {
+  if (!element) {
+    throw new Error(`Element does not exist!`);
+  }
+
+  element.click();
+  await wait(200);
+}
+
+async function pickStartTime() {
+  const timePickerLists = document.querySelectorAll(".ui-timepicker-list");
+  const startTimePickerList = timePickerLists[0];
+  const startListItems = startTimePickerList.querySelectorAll("li");
+
+  for (let i = 0; i < startListItems.length; i++) {
+    if (startListItems[i].textContent?.includes("9:00")) {
+      return await clickAndWait(startListItems[i] as HTMLElement);
+    }
+  }
+}
+
+async function pickEndTime() {
+  const timePickerLists = document.querySelectorAll(".ui-timepicker-list");
+  const endTimePickerList = timePickerLists[1];
+  const endListItems = endTimePickerList.querySelectorAll("li");
+
+  for (let i = 0; i < endListItems.length; i++) {
+    if (endListItems[i].textContent?.includes("17:00")) {
+      return await clickAndWait(endListItems[i] as HTMLElement);
+    }
+  }
+}
